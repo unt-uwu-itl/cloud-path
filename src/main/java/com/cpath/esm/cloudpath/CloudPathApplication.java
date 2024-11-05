a3a489cca44b43aece90e92dd03b2a807c7adc96
unt-uwu-itl <olegpakdev@gmail.com>
2024-11-05 22:03:12 +0500

    Initial Commit

---
+++ src/main/java/com/cpath/esm/cloudpath/CloudPathApplication.java
@@ -0,0 +1,13 @@
+package com.cpath.esm.cloudpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudPathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cpath/esm/cloudpath/CloudPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpath.esm.cloudpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
